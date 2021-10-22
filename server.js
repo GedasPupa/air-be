9807e5a02339f939f4213efc8cb5f70c31a11041
GedasPupa <gedosas@gmail.com>
2021-10-22 14:19:30 +0300

    date validation

--- server.js
+++ server.js
@@ -123,11 +123,9 @@ app.post(
     //validation:
     check("from_town").isLength({min: 3, max: 64}).withMessage("'from_town' field must be 3-64 characters long!"),
     check("airline").isLength({min: 3, max: 32}).withMessage("'airline' field must be 3-32 characters long!"),
-    // check("arrival_time").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD'."),
+    check("arrival_time").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD HH:MM'."),
     check("is_late").custom(value => (value == 0 || value == 1) ? true : false).withMessage("'is_late' field must be 0 or 1!"),
     
-    //.matches('/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/)
-
     (req, res) => {
         // validation:
         const errors = validationResult(req);
@@ -166,7 +164,7 @@ app.put(
     //validation:
     check("from_town").isLength({min: 3, max: 64}).withMessage("'from_town' field must be 3-64 characters long!"),
     check("airline").isLength({min: 3, max: 32}).withMessage("'airline' field must be 3-32 characters long!"),
-    // check("arrival_time").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD'."),
+    check("arrival_time").isISO8601().toDate().withMessage("Not valid date format! Please enter: 'YYYY-MM-DD HH:MM'."),
     check("is_late").custom(value => (value == 0 || value == 1) ? true : false).withMessage("'is_late' field must be 0 or 1!"),
     
     (req, res) => {
